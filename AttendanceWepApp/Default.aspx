a67b699907f68076cd88d3bf0bf21b9d5b80313c
Krishna Nagesh Kukkadapu <KrishnaNagesh_K@ad.infosys.com>
2018-12-14 15:10:41 +0100

    Add project files.

---
+++ AttendanceWepApp/Default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="AttendanceWepApp._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <table id="tblLoginScreen" runat="server" border="0">
+        <tr><td>&nbsp;</td></tr>
+        <tr>
+            <td align="right"><b>Login </b></td>
+            <td>
+                &nbsp;
+                <asp:TextBox ID="txtLogin" runat="server"></asp:TextBox></td>
+        </tr>
+        <tr><td>&nbsp;</td></tr>
+        <tr>
+            <td><b>Password </b></td>
+            <td>&nbsp;
+                <asp:TextBox ID="txtPwd" runat="server"></asp:TextBox></td>
+        </tr>
+        <tr><td>&nbsp;</td></tr>
+        <tr><td align="center" colspan="2"><asp:Button ID="btnLogin" runat="server" Width="100pt" Text="Hit me Hard"/></td></tr>
+    </table>
+
+</asp:Content>
---
+++ AttendanceWepApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AttendanceWepApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AttendanceWepApp")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4be64460-ecb4-43d5-b409-2100f6f63d14")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
